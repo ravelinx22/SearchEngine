32310eeebb5a4289a713092affeacc91a4f53687
William Ravelo M <drummerwilliam@gmail.com>
2021-05-30 19:33:55 -0500

    failing tests

---
+++ spec/requests/engine_controller_spec.rb
@@ -0,0 +1,19 @@
+describe EngineController, type: :request do
+  describe '#search' do
+    it 'should search on google' do
+      raise 'Error'
+    end
+
+    it 'should search on bing' do
+      raise 'Error'
+    end
+
+    it 'should search on both google and bing' do
+      raise 'Error'
+    end
+
+    it 'should not search on an invalid engine' do
+      raise 'Error'
+    end
+  end
+end
